aea32d2dfa40bebb0bfba2e5c31d95ad33f52ce9
notnoname <notnoname@gmail.com>
2021-10-28 20:08:31 +0800

    1、开始入柜

--- app/src/main/java/me/liuzs/cabinetmanager/DepositActivity.java
+++ app/src/main/java/me/liuzs/cabinetmanager/DepositActivity.java
@@ -1,21 +1,10 @@
 package me.liuzs.cabinetmanager;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
 import android.widget.TextView;
 
-import androidx.lifecycle.Lifecycle;
-
-import java.lang.ref.WeakReference;
-
-import me.liuzs.cabinetmanager.model.Cabinet;
-import me.liuzs.cabinetmanager.model.DepositItem;
 import me.liuzs.cabinetmanager.model.DepositRecord;
-import me.liuzs.cabinetmanager.model.User;
-import me.liuzs.cabinetmanager.net.APIJSON;
-import me.liuzs.cabinetmanager.net.RemoteAPI;
 
 /**
  * 初次存入模块
---
+++ app/src/main/java/me/liuzs/cabinetmanager/model/DefaultDict.java
@@ -0,0 +1,6 @@
+package me.liuzs.cabinetmanager.model;
+
+public class DefaultDict {
+    public final static int[] ContainerSpec = {5, 10, 20, 25, 50, 100};
+    public final static String[] ContainerSpecName = {"5升", "10升", "20升", "25升", "50升", "100升"};
+}
