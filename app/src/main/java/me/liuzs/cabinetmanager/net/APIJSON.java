447cd9d8ea0bd8cdfdf15fc0767585ff721de340
notnoname <notnoname@gmail.com>
2021-11-12 16:46:49 +0800

    1、优化HttpCode提示信息

--- app/src/main/java/me/liuzs/cabinetmanager/net/APIJSON.java
+++ app/src/main/java/me/liuzs/cabinetmanager/net/APIJSON.java
@@ -1,12 +1,20 @@
 package me.liuzs.cabinetmanager.net;
 
+import android.text.TextUtils;
+
 public class APIJSON<T> {
 
     @SuppressWarnings("rawtypes")
     public static APIJSON buildServerErrorJSON(int httpCode) {
         APIJSON result = new APIJSON();
         result.status = Status.error;
-        result.error = "服务器异常：" + httpCode;
+        String info = null;
+        if (httpCode == 401) {
+            info = "用户鉴权失败";
+        } else if (httpCode == 403) {
+            info = "无访问权限";
+        }
+        result.error = "请求失败:" + httpCode + (TextUtils.isEmpty(info) ? "" : "(" + info + ")");
         return result;
     }
 
@@ -14,7 +22,7 @@ public class APIJSON<T> {
     public static APIJSON buildOtherErrorJSON(Exception exception) {
         APIJSON<?> result = new APIJSON();
         result.status = Status.other;
-        result.error = "异常错误：" + exception.getMessage();
+        result.error = "异常错误:" + exception.getMessage();
         return result;
     }
 
